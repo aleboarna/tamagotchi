bbe100ab30af1b59908b9b6ad6880fbe14fed9e0
Alexandra Boarna <ale.boarna@gmail.com>
2023-07-26 13:35:31 +0300

    feat(basic-ui):added basic ui

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,12 +1,51 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import { ReactComponent as SuperWoman } from '../assets/super-woman.svg';
+import { LevelBar } from './level-bar';
+import { useEffect, useState } from 'react';
 
 export function App() {
+  const [healthLevel, setHealthLevel] = useState(45);
+  const [happinessLevel, setHappinessLevel] = useState(60);
+
+  const increaseHealth = () => {
+    setHealthLevel((prevState) => prevState + 5);
+  };
+  const increaseHappiness = () => {
+    setHappinessLevel((prevState) => prevState + 5);
+  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHappinessLevel((prevState) => prevState - 1);
+      setHealthLevel((prevState) => prevState - 1);
+    }, 3000);
+    if (happinessLevel === 0 || healthLevel === 0) {
+      return clearTimeout(timer);
+    }
+    return () => clearTimeout(timer);
+  });
   return (
-    <div>
-      <NxWelcome title="web" />
+    <div className="container mx-auto box-border w-1/3">
+      <h1 className="text-center text-xl pt-4 pb-10">Superwoman</h1>
+      <div className="flex justify-evenly">
+        <LevelBar level={healthLevel} name={'Health'} />
+        <LevelBar level={happinessLevel} name={'Happiness'} />
+      </div>
+      <SuperWoman className="container" />
+      <div className="flex justify-evenly">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          onClick={increaseHealth}
+        >
+          Feed
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          onClick={increaseHappiness}
+        >
+          Play
+        </button>
+      </div>
     </div>
   );
 }
---
+++ apps/web/src/app/level-bar.tsx
@@ -0,0 +1,20 @@
+export const LevelBar = (props: { level: number; name: string }) => {
+  return (
+    <div className={'w-1/4'}>
+      <div className="flex justify-between mb-1">
+        <span className="text-base font-medium text-blue-700 ">
+          {props.name}
+        </span>
+        <span className="text-sm font-medium text-blue-700 ">
+          {props.level}%
+        </span>
+      </div>
+      <div className="w-full bg-gray-200 rounded-full h-2.5 ">
+        <div
+          className="bg-blue-600 h-2.5 rounded-full"
+          style={{ width: `${props.level}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+};
