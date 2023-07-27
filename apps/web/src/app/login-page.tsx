4dc58ffaec3dca6f123f03b46927870eaf1750c5
Alexandra Boarna <ale.boarna@gmail.com>
2023-07-27 10:25:50 +0300

    feat(ui):ui fixes

--- apps/web/src/app/login-page.tsx
+++ apps/web/src/app/login-page.tsx
@@ -1,15 +1,17 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { ReactComponent as LoginImage } from '../assets/login-page.svg';
 
 export const LoginPage = () => {
   const [name, setName] = useState('');
   const navigate = useNavigate();
   return (
     <>
+      <LoginImage className="container w-1/4 h-1/4 pt-10" />
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            What's your name?
+            Hi! I'm Ale. What's your name?
           </h2>
         </div>
 
@@ -42,7 +44,7 @@ export const LoginPage = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Enter!
+                Let's go!
               </button>
             </div>
           </form>
