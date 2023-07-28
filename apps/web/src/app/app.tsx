7e0ae778265d7228ccdb842da299123bdcc4df21
Alexandra Boarna <ale.boarna@gmail.com>
2023-07-28 10:01:42 +0300

    feat(ui): added bottom crowd skeleton

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,18 +1,29 @@
-import { ReactComponent as SuperWoman } from '../assets/super-woman.svg';
 import { LevelBar } from './level-bar';
 import { useEffect, useRef, useState } from 'react';
 import RetryModal from './retry-modal';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { getLifeStage } from './right-lifecycle';
 import { getStageProps } from './stage-modifier';
+import { ReactComponent as People } from '../assets/people.svg';
 
 export function App() {
   const location = useLocation();
   const navigate = useNavigate();
   const { name } = location.state || {};
+
   useEffect(() => {
     if (!name) navigate('/login');
   }, [name]);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIsVisible((prevIsVisible) => !prevIsVisible);
+    }, 1800);
+
+    // Clean up the timer when the component is unmounted
+    return () => clearInterval(timer);
+  }, []);
   const [healthLevel, setHealthLevel] = useState(
     Number(localStorage.getItem(`${name}-health`)) || 100
   );
@@ -99,8 +110,8 @@ export function App() {
   };
 
   return (
-    <div>
-      <div className="flex justify-end pr-8 pt-3">
+    <div className={'flex flex-col items-center justify-between min-h-screen'}>
+      <div className="absolute right-2 pt-3">
         <Link
           className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
           to={'/login'}
@@ -113,40 +124,92 @@ export function App() {
         isOpen={isModalOpen}
         onCloseModal={() => setIsModalOpen(false)}
       />
-      <div className="container mx-auto box-border w-1/3">
-        <h1 className="text-center text-xl pt-4 pb-10">EmpowHER</h1>
-        <div className="flex justify-evenly">
-          <LevelBar level={healthLevel} name={'Health'} />
-          <LevelBar level={happinessLevel} name={'Happiness'} />
-          <LevelBar level={age} name={'Age'} />
+
+      <h1 className={' text-xl pt-4 pb-10'}>EmpowHER</h1>
+      <div className={'flex justify-evenly w-2/5 pb-7'}>
+        <LevelBar level={healthLevel} name={'Health'} />
+        <LevelBar level={happinessLevel} name={'Happiness'} />
+        <LevelBar level={age} name={'Age'} />
+      </div>
+      <div className={'flex flex-row justify-evenly'}>
+        {' '}
+        <div>leaderboard</div>
+        <div className={'w-1/2 pb-10'}>
+          <modifiers.image />
         </div>
-        <modifiers.image />
-        {/*<SuperWoman className="container" />*/}
-        {healthLevel === 0 || happinessLevel === 0 ? (
-          <div className="flex justify-center">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-              onClick={onRetryModal}
-            >
-              Try again
-            </button>
-          </div>
-        ) : (
-          <div className="flex justify-evenly">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-              onClick={healthLevel < 96 ? increaseHealth : undefined}
-            >
-              Support
-            </button>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-              onClick={happinessLevel < 96 ? increaseHappiness : undefined}
-            >
-              Empower
-            </button>
-          </div>
-        )}
+        <div>story</div>
+      </div>
+
+      {healthLevel === 0 || happinessLevel === 0 ? (
+        <div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={onRetryModal}
+          >
+            Try again
+          </button>
+        </div>
+      ) : (
+        <div className={'flex flex-row justify-around w-1/4'}>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={healthLevel < 96 ? increaseHealth : undefined}
+          >
+            Support
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={happinessLevel < 96 ? increaseHappiness : undefined}
+          >
+            Empower
+          </button>
+        </div>
+      )}
+      <div className={'relative bottom-0 flex flex-row w-full '}>
+        <div className={`  absolute w-full flex justify-evenly`}>
+          <p
+            className={`text-xl font-bold italic  ease-in-out duration-100 delay-300  ${
+              isVisible ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            Stop making a mess
+          </p>
+          <p
+            className={`text-xl font-bold italic  ease-in-out duration-100 delay-200  ${
+              !isVisible ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            Why aren't you listening
+          </p>
+          <p
+            className={`text-xl font-bold italic ease-in-out duration-100 delay-100 ${
+              isVisible ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            You're a cry baby
+          </p>
+          <p
+            className={`text-xl font-bold italic ease-in-out duration-100 ${
+              !isVisible ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            You talk too much
+          </p>
+          <p
+            className={`text-xl font-bold italic ease-in-out duration-100 delay-200  ${
+              isVisible ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            Why can't you be more like your sister
+          </p>
+        </div>
+        <People></People>
+        <People></People>
+        <People></People>
+        <People></People>
+        <People></People>
+        <People></People>
+        <People></People>
       </div>
     </div>
   );
