fbca85b895ce841cee10ccb907033e25e406d1a9
Alexandra Boarna <ale.boarna@gmail.com>
2023-07-29 12:45:25 +0300

    feat(aws-cdk):added cloudfront distribution + s3

--- apps/infra/src/bin/app.ts
+++ apps/infra/src/bin/app.ts
@@ -4,11 +4,14 @@ import * as cdk from 'aws-cdk-lib';
 import { DbStack } from '../lib/db-stack';
 import { LambdaStack } from '../lib/lambda-stack';
 import { ApiStack } from '../lib/api-stack';
+import { WebStack } from '../lib/web-stack';
 
 const app = new cdk.App();
 
 const projectName = 'empowher';
 const stackEnv = app.node.tryGetContext('stackEnv');
+const domainName = app.node.tryGetContext('domainName');
+const domainCertificateWeb = app.node.tryGetContext('domainCertificateWeb');
 
 const db = new DbStack(app, `${projectName}-DB-${stackEnv}`, {
   // https://docs.aws.amazon.com/cdk/v2/guide/environments.html
@@ -44,3 +47,16 @@ const api = new ApiStack(app, `${projectName}-API-${stackEnv}`, {
 });
 
 api.addDependency(lambda);
+
+const web = new WebStack(app, `${projectName}-Web-${stackEnv}`, {
+  // https://docs.aws.amazon.com/cdk/v2/guide/environments.html
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  projectName: projectName,
+  description: `Contains the Web tier resources`,
+  domainName,
+  domainCertificateWeb,
+  stackEnv,
+});
