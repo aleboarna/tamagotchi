4034c2961a2fc782c765c023ac87d87892109275
Alexandra Boarna <ale.boarna@gmail.com>
2023-07-31 10:24:23 +0300

    feat(routing): redirect to login if path is other

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './app/app';
 import { LoginPage } from './app/login-page';
+
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,6 +17,7 @@ root.render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path={'*'} element={<Navigate to={'/login'} />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
